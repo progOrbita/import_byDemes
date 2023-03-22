7879812f09e01c7646d8f0d91b39c62decfcf7cf
progOrbita <web.orbitadigital@gmail.com>
2023-03-22 13:00:21 +0100

    Update byDemes.php
    add constructor

--- src/byDemes.php
+++ src/byDemes.php
@@ -12,4 +12,14 @@ class ByDemes
     private $fieldsMultilingual = [];
     private $lastError = "";
 
+    public function __construct(string $csvLink, array $header, array $nameCSVs, array $fieldsMultilingual, string $key = "supplier_reference")
+    {
+        $this->csvLink = $csvLink;
+        $this->header = $header;
+        $this->nameCSVs = $nameCSVs;
+        $this->lang = array_keys($nameCSVs);
+        $this->key = $key;
+        $this->fieldsMultilingual = $fieldsMultilingual;
+    }
+
 }
