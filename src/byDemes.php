f0edfe7ec78d7d85414ce569100c54e65a63cffe
progOrbita <web.orbitadigital@gmail.com>
2023-03-22 13:00:08 +0100

    Update byDemes.php
    declare class vars

--- src/byDemes.php
+++ src/byDemes.php
@@ -3,4 +3,13 @@ require_once(_PS_CORE_DIR_ . '/import/files/src/CsvImporter.php');
 
 class ByDemes
 {
+    private $lang = [];
+    private $csvLink = "";
+    private $data = [];
+    private $header = [];
+    private $nameCSVs = [];
+    private $key = "";
+    private $fieldsMultilingual = [];
+    private $lastError = "";
+
 }
