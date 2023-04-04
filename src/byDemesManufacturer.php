0767fd7c7577d5ffad5b3f4cf3623274039d73e0
progOrbita <web.orbitadigital@gmail.com>
2023-04-04 10:54:24 +0200

    Update byDemesManufacturer.php
    create get() function to return manufacturer´s id

--- src/byDemesManufacturer.php
+++ src/byDemesManufacturer.php
@@ -2,4 +2,27 @@
 require_once(_PS_CORE_DIR_ . '/config/config.inc.php');
 
 class ByDemesManufacturer
+{
+    /**
+     * get manufacturer id if brand doesnt exist we create it 
+     * 
+     * @param string $name 
+     * 
+     * @return int
+     */
+    public static function get(string $name): int
+    {
+        if (empty($name)) {
+            return 0;
+        }
+
+        $name = self::formatName($name);
+        $id = Manufacturer::getIdByName($name);
+        if ($id) {
+            return $id;
+        }
+
+        return self::createManufacturer($name);
+    }
+
 }
