2b7a8a9bebb79ee1876b721259f06c37b390529d
progOrbita <web.orbitadigital@gmail.com>
2023-04-04 11:06:43 +0200

    Update index.php
    add html code

--- index.php
+++ index.php
@@ -30,3 +30,27 @@ if ($ps_data === false) {
 }
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.js">
+    </script>
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js">
+    </script>
+    <title>byDemes</title>
+</head>
+
+<body>
+    <?php
+    echo Table::makeTable($res, ['Refencia de proveedor', 'Resultado']);
+    ?>
+</body>
+
+
+</html>
