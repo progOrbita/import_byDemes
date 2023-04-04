8265565c7da8efb8fc84808bf9a07ff245a8fdf4
progOrbita <web.orbitadigital@gmail.com>
2023-04-04 10:56:11 +0200

    Update byDemesCategories.php
    declare class vars

--- src/byDemesCategories.php
+++ src/byDemesCategories.php
@@ -4,4 +4,9 @@ require_once(_PS_CORE_DIR_ . '/import/files/src/JsonImporter.php');
 
 class ByDemesCategories
 {
+    private $data = [];
+    private $name = _PS_CORE_DIR_ . '/import/byDemes/data/byDemesJsonCategories.json';
+    private $lastError = '';
+    private $originalData = [];
+
 }
