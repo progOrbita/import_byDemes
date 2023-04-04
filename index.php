c1d25d0eee3342600b6fa3d78def172778b54312
progOrbita <web.orbitadigital@gmail.com>
2023-04-04 11:03:10 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once '../../config/config.inc.php';
+require_once __DIR__ . '/src/byDemes.php';
+require_once __DIR__ . '/src/byDemesManufacturer.php';
+require_once __DIR__ . '/src/byDemesCategories.php';
+require_once __DIR__ . '/src/actions.php';
+require_once(_PS_CORE_DIR_ . '/import/files/src/Table.php');
+
+
+?>
+
