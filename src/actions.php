923f353cd4056014dc734ea95cc08e2b0c44b5e1
progOrbita <web.orbitadigital@gmail.com>
2023-05-08 09:20:01 +0200

    Update actions.php
    add new function createImg to create products imgs

--- src/actions.php
+++ src/actions.php
@@ -195,4 +195,36 @@ class Actions extends AdminImportControllerCore
     {
         return abs($n1 - $n2) < $diff;
     }
+
+    /**
+     * create img product
+     * 
+     * @param int $idProduct
+     * @param string $url
+     * @param array $legend
+     * 
+     * @return bool
+     */
+    public static function createImg(int $idProduct, string $url, array $legend)
+    {
+        $image = new Image();
+        $image->id_product = (int) $idProduct;
+        $image->position = Image::getHighestPosition($idProduct) + 1;
+        $image->legend = $legend;
+        if (!Image::getCover($image->id_product)) {
+            $image->cover = 1;
+        } else {
+            $image->cover = 0;
+        }
+
+        if (($image->validateFieldsLang(false, true)) !== true) {
+            return false;
+        }
+
+        if (!$image->add()) {
+            return false;
+        }
+
+        return (parent::copyImg($idProduct, $image->id, $url));
+    }
 }
